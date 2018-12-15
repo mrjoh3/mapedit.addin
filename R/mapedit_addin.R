2c583864e896b31992015e502a83d989d6ac1173
Matt Johnson <mrjoh3@gmail.com>
2018-12-15 20:50:45 +1100

    object and filenames can now be modified. closes issue #1, closes issue #2

--- R/mapedit_addin.R
+++ R/mapedit_addin.R
@@ -8,9 +8,9 @@
 #' @importFrom miniUI miniPage miniContentPanel gadgetTitleBar miniButtonBlock
 #' @import mapedit
 #' @importFrom shiny callModule paneViewer observeEvent stopApp runGadget textInput updateTextInput div
-#' @importFrom shinyWidgets switchInput
+#' @importFrom shinyWidgets switchInput updateSwitchInput
 #' @importFrom mapview mapview
-#' @importFrom sf write_sf
+#' @importFrom sf write_sf st_intersection
 #' @importFrom leaflet setView
 #' @importFrom rstudioapi getActiveDocumentContext
 #' @export
@@ -39,12 +39,16 @@ mapeditAddin <- function() {
     # get values from rstudio
     ct <- getActiveDocumentContext()
 
-    TEXT <<- ct$selection[[1]]$text
+    TEXT <- ct$selection[[1]]$text
     OBJECTNAME <- ifelse(TEXT == '', 'geom', TEXT)
     FILENAME <- ifelse(TEXT == '', 'saved_geometry.geojson', paste0(TEXT, '.geojson'))
     SF_OBJECT <- NULL
 
-    try(SF_OBJECT <- get(TEXT, silent = TRUE))
+    # test selected text an sf object
+    try({
+      SF_OBJECT <- get(TEXT)
+      if (class(SF_OBJECT) != 'sf') {SF_OBJECT <- NULL}
+    })
 
     # update UI based on inputs
     updateTextInput(session, 'filename', value = FILENAME)
@@ -65,12 +69,12 @@ mapeditAddin <- function() {
     observeEvent(input$done, {
       geom <- geo()$finished
 
-      if (!is.null(geom) & !is.null(SF_OBJECT)) geom <- st_intersection(SF_OBJECT, geom)
+      if (!is.null(geom) & !is.null(SF_OBJECT)) geom <- rbind(SF_OBJECT, geom)
 
       if (!is.null(geom)) {
         assign(OBJECTNAME, geom, envir = .GlobalEnv)
         if (input$savefile) {
-          sf::write_sf(geom, input$filename, update = TRUE)
+          sf::write_sf(geom, input$filename, delete_layer = TRUE, delete_dsn = TRUE)
         }
       }
 
